35e6d5aa8f72de316b511feaea293efd1bbd2d4d
ISheep <lzyoutlook@gmail.com>
2020-11-12 20:18:36 +0800

    将项目磨模块化\

---
+++ mailserver/src/test/java/com/codelin/MailserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
